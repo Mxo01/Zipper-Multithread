0f6ad8d2d7147b8f1d189ca7acbc4f7140a27285
Mario Di Modica <mariodimodica.01@gmail.com>
2022-09-17 18:38:52 +0200

    ThreadPool version update

--- Main.java
+++ Main.java
@@ -1,21 +1,25 @@
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
   public static void main(String args[]) {
+    ExecutorService service = Executors.newCachedThreadPool(); // create a new thread pool
     for (int i=0; i<args.length; i++) {
       File directory = new File(args[i]); // directory path
       if (directory.exists() && directory.isDirectory()) { // check if directory
         File[] fileList = directory.listFiles(); // list files inside directory
-        RecZip(fileList, directory);
+        RecZip(service, fileList, directory);
       }
       else {
         System.err.println("'" + directory + "'" + " isn't a directory!");
       }
     }
+    service.shutdown(); // shut down the thread pool
   }
   
   // Recursively check inside directory
-  public static void RecZip(File[] listDir, File stdDir) {
+  public static void RecZip(ExecutorService service, File[] listDir, File stdDir) {
     for (File file : listDir) {
       if (file.isFile()) { // check if is file 
         if (!file.getName().endsWith(".zip")) { // check if it's already a zipped file
@@ -23,14 +27,13 @@ public class Main {
           String fileName = file.getName(); // current file name
           String destination = fileName.substring(0, fileName.lastIndexOf('.')) + ".zip"; // zipped file destination
           ZipThread zippedFile = new ZipThread(filePath, stdDir + "\\" + destination); // create zipped file
-          Thread thread = new Thread(zippedFile); // create thread
-          thread.start(); // starting thread
+          service.execute(zippedFile); // starting thread of ThreadPool
         }
       }
       if (file.isDirectory()) { // check if is directory
         File newDir = new File(file.getAbsolutePath());
         File[] files = file.listFiles(); // list files inside directory
-        RecZip(files, newDir);
+        RecZip(service, files, newDir);
       }
     }
   }
